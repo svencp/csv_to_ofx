69e8839c2050fb6e0f4e4be3af3df3b0f1d5dff5
svencp <svenponelat@hotmail.com>
2024-05-04 22:14:10 -0300

    launch working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// Author: Sven Ponelat
+// Date:   2024-05-02
+//
+// This is my app to convert csv files to an ofx file
+// typically from the EQ bank
+//
+//
+
+
+
+
 fn main() {
     println!("Hello, Svenny!");
+    println!("Hello, Svenny! again");
+    println!("Hello, Svenny! again again");
 }
